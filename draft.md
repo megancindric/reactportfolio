e3a209a09bf6254b1ee96b2a51036091811d58cb
Megan Cindric <megancindric@gmail.com>
2023-02-27 19:50:39 -0600

    📦 NEW: Created base blog page with placeholder search & categories

---
+++ draft.md
@@ -0,0 +1,18 @@
+# In Defense of Death
+
+Content Warning: This post will deal heavily with death, dying, and grief
+
+My step-dad passed away the day after Christmas.
+
+I just wanted to start with that.  No sense beating around the bush or tiptoing around it. 
+
+And it hurts.  It hurts more than anything I've experienced so far in my life.  I keep waiting for it to stop, for it to get better, but it isn't.
+And maybe it's not supposed to.
+
+We don't talk about death much in our culture.  It's a very hush-hush, solumn sort of affair.  
+
+I remember the first time I really, truly thought about my parents dying.  I sat with that feeling, chewed on it as long as I could, and decided it was
+too unpleasant to complicate.  It filled me with dread, that one day they might be gone, that I would have to live without them.
+
+We will all die.  Every parent, every friend, every pet, every single living thing must die.  The moment we are born, we are stamped with an expiration
+date with the words "TBD".  Life implies death.  So surely, there must be a reason for death.
--- src/pages/BlogPage/BlogPage.jsx
+++ src/pages/BlogPage/BlogPage.jsx
@@ -1,10 +1,100 @@
 import React from "react";
-import { Container } from "@mui/material";
+import {
+  Box,
+  Container,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Bob from "../../assets/garden3.jpg";
 
 const BlogPage = (props) => {
   return (
-    <Container>
-      <h1>This is the blog page!</h1>
+    <Container
+      sx={{
+        display: "flex",
+        margin: "3rem",
+
+        gap: "3rem",
+      }}
+    >
+      <Box sx={{ flexBasis: "70%" }}>
+        <Box
+          sx={{
+            padding: "1rem",
+            height: "300px",
+            width: "900px",
+            mb: "1rem",
+            backgroundImage: `url(${Bob})`,
+          }}
+        ></Box>
+        <Box sx={{ padding: "1rem" }}>
+          <Typography variant="h4" mb={1}>
+            This is the post title!
+          </Typography>
+          <Typography variant="overline" gutterBottom>
+            And a very small date
+          </Typography>
+          <Typography variant="body1">And the main content!</Typography>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          flexBasis: "30%",
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          sx={{
+            border: "1px solid",
+            borderRadius: "10px",
+            borderColor: "#43A047",
+            padding: "1rem",
+            mb: "1rem",
+          }}
+        >
+          <Typography variant="h5" gutterBottom sx={{ mb: "1rem" }}>
+            Search
+          </Typography>
+
+          <TextField id="outlined-basic" size="small" />
+        </Box>
+        <Box
+          sx={{
+            padding: "1rem",
+            border: "1px solid",
+            borderRadius: "10px",
+            borderColor: "#43A047",
+          }}
+        >
+          <Typography variant="h5">Categories</Typography>
+          <List>
+            <ListItem>
+              <ListItemButton>
+                <ListItemText primary="Coding" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton>
+                <ListItemText primary="Thailand" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton>
+                <ListItemText primary="Travels" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton>
+                <ListItemText primary="Business" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
+      </Box>
     </Container>
   );
 };
