a13aa7e3a72104df5126fe1967ddddca447e1d3f
Megan Cindric <megancindric@gmail.com>
2023-02-28 19:54:28 -0600

    📦 NEW: Created Project Card component for ProjectPage

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,31 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
+const ProjectCard = ({ project }) => {
+  return (
+    <Card sx={{ maxWidth: 350 }}>
+      <CardContent>
+        <Typography variant="h5" gutterBottom>
+          {project.name}
+        </Typography>
+        {project.technologies.map((tech) => (
+          <Typography variant="overline">{tech} </Typography>
+        ))}
+        <Typography variant="body2" gutterBottom>
+          {project.description}
+        </Typography>
+        <Button size="small" variant="outlined">
+          Source Code
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ProjectCard;
--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,10 +1,50 @@
+import { Card, Container, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
 
 const ProjectPage = (props) => {
+  const projects = [
+    {
+      name: "Walkabout",
+      description:
+        "Some fun placeholder text about this really cool project I made",
+      technologies: ["Python", "Django"],
+      repository: "http://www.google.com",
+    },
+    {
+      name: "Walkabout",
+      description:
+        "Some fun placeholder text about this really cool project I made",
+      technologies: ["Python", "Django"],
+      repository: "http://www.google.com",
+    },
+    {
+      name: "Walkabout",
+      description:
+        "Some fun placeholder text about this really cool project I made",
+      technologies: ["Python", "Django"],
+      repository: "http://www.google.com",
+    },
+    {
+      name: "Walkabout",
+      description:
+        "Some fun placeholder text about this really cool project I made",
+      technologies: ["Python", "Django"],
+      repository: "http://www.google.com",
+    },
+  ];
   return (
-    <div className="container">
-      <h1>This is the projects page!</h1>
-    </div>
+    <Container sx={{ mt: "1rem" }}>
+      <Typography variant="h3" sx={{ mb: "2rem" }}>
+        This is the projects page!
+      </Typography>
+      <Box sx={{ display: "flex", flexWrap: "row", gap: "1rem" }}>
+        {projects.map((project) => (
+          <ProjectCard project={project} />
+        ))}
+      </Box>
+    </Container>
   );
 };
 
