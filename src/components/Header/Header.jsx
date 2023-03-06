09de80f6a8bd492cb1d02368d96eb68a8f201444
Megan Cindric <megancindric@gmail.com>
2023-03-06 12:04:04 -0600

    👌 IMPROVE: Adjusted position of items in header navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 import ForestIcon from "@mui/icons-material/Forest";
-import { Container } from "@mui/system";
 import { useNavigate } from "react-router-dom";
 import React from "react";
 
@@ -13,46 +12,49 @@ const Header = (post) => {
   const navigate = useNavigate();
   return (
     <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <ForestIcon
-            sx={{ display: "flex", mr: 1 }}
-            onClick={() => navigate(`/`)}
-          />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: "flex",
-              fontFamily: "Segoe UI",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            Code & Explore
-          </Typography>
+      <Toolbar disableGutters>
+        <ForestIcon
+          sx={{ display: "flex", ml: 2 }}
+          onClick={() => navigate(`/`)}
+        />
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            mr: 2,
+            display: "flex",
+            fontFamily: "Segoe UI",
+            fontWeight: 700,
+            letterSpacing: ".3rem",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          Code & Explore
+        </Typography>
 
-          <Box
-            sx={{ flexGrow: 1, display: "flex", justifyContent: "flex-end" }}
-          >
-            {pages.map((page) => (
-              <Button
-                size="large"
-                key={page.link}
-                onClick={() => navigate(`/${page.link}`)}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page.button}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </Container>
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: "flex",
+            justifyContent: "flex-end",
+            mr: 3,
+          }}
+        >
+          {pages.map((page) => (
+            <Button
+              size="large"
+              key={page.link}
+              onClick={() => navigate(`/${page.link}`)}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              {page.button}
+            </Button>
+          ))}
+        </Box>
+      </Toolbar>
     </AppBar>
   );
 };
